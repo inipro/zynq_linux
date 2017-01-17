977ba0aa5c0d7c1ca1e73c409f04055f095ef155
Hyunok Kim <hokim1972@naver.com>
2017-01-18 06:19:31 +0900

    Fix uboot

--- u-boot/zynq_zyboc.h
+++ u-boot/zynq_zyboc.h
@@ -23,6 +23,19 @@
 /* Define ZYBO PS Clock Frequency to 50MHz */
 #define CONFIG_ZYNQ_PS_CLK_FREQ	50000000UL
 
+#define CONFIG_EXTRA_ENV_SETTINGS   \
+    "loadbootenv_addr=0x2000000\0" \
+    "bootenv=uEnv.txt\0" \
+    "loadbootenv=load mmc 0 ${loadbootenv_addr} ${bootenv}\0" \
+    "importbootenv=echo Importing environment from SD ...; " \
+        "env import -t ${loadbootenv_addr} $filesize\0" \
+    "sd_uEnvtxt_existence_test=test -e mmc 0 /uEnv.txt\0" \
+    "preboot=if test $modeboot = sdboot && env run sd_uEnvtxt_existence_test; " \
+            "then if env run loadbootenv; " \
+                "then env run importbootenv; " \
+            "fi; " \
+        "fi; \0"
+
 #include <configs/zynq-common.h>
 
 #endif /* __CONFIG_ZYNQ_ZYBOC_H */
