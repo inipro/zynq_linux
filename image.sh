e85f4e22452f1d506e9e6cf8f75b62c87c316154
Hyunok Kim <hokim1972@naver.com>
2017-11-20 19:27:36 +0900

    Update image.sh

--- image.sh
+++ image.sh
@@ -7,23 +7,8 @@ if [ ! -f dl/$UBUNTU ]; then
 	wget $UBUNTU_URL/$UBUNTU -O dl/$UBUNTU
 fi
 
-ARCH=armhf
-SIZE=3500
-mkdir -p img
-IMAGE=img/ubuntu-core_${ARCH}_16.04.img
-dd if=/dev/zero of=$IMAGE bs=1M count=$SIZE
-DEVICE=$(losetup -f)
-losetup $DEVICE $IMAGE
-parted -s $DEVICE mklabel msdos
-parted -s $DEVICE mkpart primary fat16 4MB 128MB
-parted -s $DEVICE mkpart primary ext4 128MB 100%
-BOOT_DEV=/dev/$(lsblk -lno NAME $DEVICE | sed '2!d')
-ROOT_DEV=/dev/$(lsblk -lno NAME $DEVICE | sed '3!d')
-mkfs.vfat -v $BOOT_DEV
-mkfs.ext4 -F -j $ROOT_DEV
 ROOT_DIR=root
 mkdir -p $ROOT_DIR
-mount $ROOT_DEV $ROOT_DIR
 cd $ROOT_DIR
 tar xvf ../dl/$UBUNTU
 rm -fr boot
@@ -40,10 +25,10 @@ cp /usr/bin/qemu-arm-static $ROOT_DIR/usr/bin/
 chroot $ROOT_DIR << EOF_CHROOT
 apt update
 apt -y upgrade
-DEBIAN_FRONTEND=noninteractive apt -y install lsb-release bc vim nano sudo openssh-server udev usbutils u-boot-tools device-tree-compiler kmod net-tools resolvconf wpasupplicant parted rfkill lshw wireless-tools gcc g++ cmake python git i2c-tools iputils-ping lxterminal leafpad linux-firmware
+DEBIAN_FRONTEND=noninteractive apt -y install lsb-release bc vim nano sudo openssh-server udev usbutils u-boot-tools device-tree-compiler kmod net-tools resolvconf wpasupplicant parted rfkill lshw wireless-tools gcc g++ cmake python git i2c-tools iputils-ping lxterminal leafpad linux-firmware tzdata locales ethtool
 echo "Asia/Seoul" > /etc/timezone
 ln -fs /usr/share/zoneinfo/Asia/Seoul /etc/localtime
-locale-gen "en_US.UTF-8"
+locale-gen en_US.UTF-8 ko_KR.UTF-8
 DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
 apt clean
 EOF_CHROOT
@@ -65,6 +50,29 @@ ExecStart=
 ExecStart=-/sbin/agetty --autologin root -s %I 115200,38400,9600 linux
 EOF_CAT
 
-umount -l $ROOT_DIR
-rmdir $ROOT_DIR
+ARCH=armhf
+SIZE=3500
+mkdir -p img
+TARGET=img/ubuntu-core_${ARCH}_16.04
+dd if=/dev/zero of=$TARGET.img bs=1M count=$SIZE
+DEVICE=$(losetup -f)
+losetup $DEVICE $TARGET.img
+parted -s $DEVICE mklabel msdos
+parted -s $DEVICE mkpart primary fat16 4MB 128MB
+parted -s $DEVICE mkpart primary ext4 128MB 100%
+BOOT_DEV=${DEVICE}p1
+ROOT_DEV=${DEVICE}p2
+mkfs.vfat -v $BOOT_DEV
+mkfs.ext4 -F -j $ROOT_DEV
+
+_ROOT_DIR=_root
+mkdir -p $_ROOT_DIR
+mount $ROOT_DEV $_ROOT_DIR
+cp -a $ROOT_DIR/* $_ROOT_DIR
+umount -l $_ROOT_DIR
+rmdir $_ROOT_DIR
+cd $ROOT_DIR
+tar cvzf ../$TARGET.tgz *
+cd ..
+rm -fr $ROOT_DIR
 losetup -d $DEVICE
